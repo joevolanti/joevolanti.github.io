8bc204243cef49ad68b5d4edc74ade8c4455290e
Joe Clifford <joe@volanti.aero>
2018-12-23 16:20:04 +0000

    no message

---
+++ dashboard.php
@@ -0,0 +1,270 @@
+<!doctype html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+
+    <title>Dashboard .::. Volanti </title>
+
+    <meta name="description" content="#">
+    <meta name="author" content="Volanti">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <!-- Stylesheets -->
+    <!-- Tailwind CSS Framework (CDN For Local Use) -->
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
+
+    <!--Font Awesome -->
+   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+	<style>
+		.poppins-family{
+			font-family: 'Poppins';
+		};
+
+    .w-20{
+      width: 5rem;
+    }
+
+    .h-20{
+      width: 5rem;
+    }
+
+    td{
+      padding:10px;
+    }
+	</style>
+
+    <!-- END Stylesheets -->
+
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+
+</head>
+
+
+<body class="poppins-family antialiased h-screen bg-grey-lighter flex">
+ 
+<!-- Sidebar -->
+<div class="bg-black text-white flex-none w-64 hidden md:block h-screen">
+
+  <!-- Logo Container -->
+  <div class="flex items-center justify-center p-6">
+    <img class="h-10" src="assets/img/logo_white.png" alt="Volanti Logo"/>
+  </div>
+
+  <!-- User Profile Window -->
+  <div class="block flex-none bg-grey-darkest text-white mt-2 p-6">
+
+    <div class="flex items-center justify-center mb-5">
+        <img class="rounded-full w-16 h-16" src="assets/img/avatar.jpg" alt="User Avatar"/>
+    </div>
+
+    <div class="text-center text-white font-normal text-sm">
+      Joe Clifford
+      <i class="fas fa-sign-out-alt"></i>
+    </div>
+    
+  </div>
+
+  <!-- Nav Bar -->
+  <nav class="block flex-none justify-center mt-10 text-grey-light font-normal text-xs">
+
+    <div class="mb-3 py-3 px-6 border-l-4 border-solid border-blue text-white font-semibold">
+      <i class="fas fa-home pr-3"></i>
+      Dashboard
+    </div>
+
+    <div class="mb-3 py-3 px-6 border-l-4 border-solid border-black hover:text-white hover:border-grey-light">
+      <i class="fas fa-user-tie pr-3"></i>
+      Staff
+    </div>
+
+    <div class="mb-3 py-3 px-6 border-l-4 border-solid border-black hover:text-white hover:border-grey-light">
+      <i class="fas fa-users pr-3"></i>
+      Members
+    </div>
+
+    <div class="mb-3 py-3 px-6 border-l-4 border-solid border-black hover:text-white hover:border-grey-light">
+      <i class="fas fa-credit-card pr-3"></i>
+      Payments
+    </div>
+
+    <div class="mb-3 py-3 px-6 border-l-4 border-solid border-black hover:text-white hover:border-grey-light">
+      <i class="fas fa-plane pr-3"></i>
+      Fleets
+    </div>
+
+    <div class="mb-3 py-3 px-6 border-l-4 border-solid border-black hover:text-white hover:border-grey-light">
+      <i class="far fa-calendar-alt pr-3"></i>
+      Bookings
+    </div>
+
+    <div class="mb-3 py-3 px-6 border-l-4 border-solid border-black hover:text-white hover:border-grey-light">
+      <i class="fas fa-cogs pr-3"></i>
+      Settings
+    </div>
+
+  </nav>
+
+
+
+</div>
+<!--Sidebar END-->
+
+<!--Site Content -->
+<div class="flex-1 flex flex-col w-full overflow-hidden">
+  <div class="conatiner p-10">
+    
+    <!-- News Notice DANGER -->
+    <div class="border-solid border-2 border-red-lighter bg-red-lightest p-5">
+
+      <h5 class="text-red">Important Notice</h5>
+
+        <br/>
+
+      <p class="text-xs">Cupcake ipsum dolor sit amet I love cotton candy lemon drops gummies. Candy canes lemon drops bear claw fruitcake lemon drops chocolate cake tart bear claw.  Danish ice cream icing pie candy canes liquorice candy.</p>
+
+    </div>
+
+    <div class="flex mt-10 -mx-5">
+
+      <!-- Todays Flights -->
+        <div class="rounded w-1/2 mx-5 bg-white border border-grey-light">
+          
+          <div class="rounded-t bg-grey-lighter p-3">
+            <header class="text-sm font-bold text-grey-darkest">Todays Flights</header>
+          </div>
+
+          <div class="p-5">
+            <table class="w-full">
+              <!-- Joe Row -->
+              <tr class="hover:bg-blue-lightest">
+                <!-- Time Pill -->
+                <td class="text-center justify-center">
+                    <p class="w-16 rounded-full py-2 px-4 bg-grey-light text-xs">13:00</p>
+                </td>
+
+                <!-- Booked By & Hire Type -->
+                <td class="leading-tight">
+                  <div class="text-sm text-grey-darkest">
+                    Joe Clifford 
+                  </div>
+
+                  <div class="text-xs text-grey">
+                    Private Hire
+                  </div> 
+                </td>
+
+                <!-- Aircraft Reg & Type -->
+                <td class="leading-tight">
+                  <div class="text-sm text-grey-darkest">
+                    G-JOES
+                  </div>
+
+                  <div class="text-xs text-grey">
+                    Seneca V | MEP
+                  </div> 
+                </td>
+
+                <td>Action</td>
+              </tr>
+
+              <!-- Calum Row -->
+              <tr class="bg-grey-lightest hover:bg-blue-lightest">
+                <!-- Time Pill -->
+                <td class="text-center justify-center">
+                    <p class="w-16 rounded-full py-2 px-4 bg-grey-light text-xs">13:45</p>
+                </td>
+
+                <!-- Booked By & Hire Type -->
+                <td class="leading-tight">
+                  <div class="text-sm text-grey-darkest">
+                    Calum Towers 
+                  </div>
+
+                  <div class="text-xs text-grey">
+                    Trial Lesson
+                  </div> 
+                </td>
+
+                <!-- Aircraft Reg & Type -->
+                <td class="leading-tight">
+                  <div class="text-sm text-grey-darkest">
+                    G-CALM
+                  </div>
+
+                  <div class="text-xs text-grey">
+                    Cessna 152 | SEP
+                  </div> 
+                </td>
+
+                <td>Action</td>
+              </tr>
+
+              <!-- Joe Row -->
+              <tr class="hover:bg-blue-lightest">
+                <!-- Time Pill -->
+                <td class="text-center justify-center">
+                    <p class="w-16 rounded-full py-2 px-4 bg-grey-light text-xs">14:30</p>
+                </td>
+
+                <!-- Booked By & Hire Type -->
+                <td class="leading-tight">
+                  <div class="text-sm text-grey-darkest">
+                    Anthony Lawrence 
+                  </div>
+
+                  <div class="text-xs text-grey">
+                    Private Hire
+                  </div> 
+                </td>
+
+                <!-- Aircraft Reg & Type -->
+                <td class="leading-tight">
+                  <div class="text-sm text-grey-darkest">
+                    G-ANTS
+                  </div>
+
+                  <div class="text-xs text-grey">
+                    Robin DR2200 | SEP
+                  </div> 
+                </td>
+
+                <td>Action</td>
+              </tr>
+
+            </table>
+          </div>
+
+        </div>
+
+      <!-- Booked Out Flights -->
+
+        <div class="rounded w-1/2 mx-5 bg-white border border-grey-light">
+          
+          <div class="rounded-t bg-grey-lighter p-3">
+            <header class="text-sm font-bold text-grey-darkest">Booked Out Aircraft</header>
+          </div>
+
+          <div class="p-3">
+            Right
+          </div>
+
+        </div>
+
+    </div>
+
+
+
+
+  </div>
+</div>
+
+	
+
+</body>
+
+</html>
